650486544e6de71e22720f673af0d48bbb493ca2
HoangAn442002 <Nguyenphamhoangan2002@gmail.com>
2023-12-22 19:57:25 +0700

    init project

---
+++ android/app/src/main/kotlin/com/example/web_dat_hang/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.web_dat_hang
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/firebase_options.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return FirebaseOptions(
+      apiKey: 'AIzaSyAIT1GP0w0bfLNDDiUq2DR93uAExTXGQUU',
+      authDomain: 'YOUR_AUTH_DOMAIN',
+      projectId: 'dathang-fafef',
+      storageBucket: 'dathang-fafef.appspot.com',
+      messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+      appId: '1:444843594711:android:0b70eac1adc5743ff23192',
+    );
+  }
+}
